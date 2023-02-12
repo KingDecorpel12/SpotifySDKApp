e775dee59e74e196914c3033869dce182546b104
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2023-02-12 13:49:07 -0500

    Add files via upload

---
+++ SpotifyTest/SpotifyTest/Constants.swift
@@ -0,0 +1,38 @@
+//
+//  Constants.swift
+//  SpotifyTest
+//
+//  Created by Omar Hegazy on 2/12/23.
+//
+
+import Foundation
+
+let accessTokenKey = "access-token-key"
+let redirectUri = URL(string:"SpotifyTest://")!
+let spotifyClientId = "457af231685947228b5c33d5a13b86b1"
+let spotifyClientSecretKey = "d1bc0ea6d5164761836cc66fe688f749"
+
+/*
+Scopes let you specify exactly what types of data your application wants to
+access, and the set of scopes you pass in your call determines what access
+permissions the user is asked to grant.
+For more information, see https://developer.spotify.com/web-api/using-scopes/.
+*/
+let scopes: SPTScope = [
+                            .userReadEmail, .userReadPrivate,
+                            .userReadPlaybackState, .userModifyPlaybackState, .userReadCurrentlyPlaying,
+                            .streaming, .appRemoteControl,
+                            .playlistReadCollaborative, .playlistModifyPublic, .playlistReadPrivate, .playlistModifyPrivate,
+                            .userLibraryModify, .userLibraryRead,
+                            .userTopRead, .userReadPlaybackState, .userReadCurrentlyPlaying,
+                            .userFollowRead, .userFollowModify,
+                        ]
+let stringScopes = [
+                        "user-read-email", "user-read-private",
+                        "user-read-playback-state", "user-modify-playback-state", "user-read-currently-playing",
+                        "streaming", "app-remote-control",
+                        "playlist-read-collaborative", "playlist-modify-public", "playlist-read-private", "playlist-modify-private",
+                        "user-library-modify", "user-library-read",
+                        "user-top-read", "user-read-playback-position", "user-read-recently-played",
+                        "user-follow-read", "user-follow-modify",
+                    ]
